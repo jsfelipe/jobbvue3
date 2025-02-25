0ac4cb011d053b6635a824955ba60790ada44581
Ricardo Alves <ricardoalv.88@gmail.com>
2025-02-25 15:26:07 -0300

    feat:install element-plus

--- src/views/Cadastros/Empresas/Listar.vue
+++ src/views/Cadastros/Empresas/Listar.vue
@@ -1,241 +1,65 @@
 <template>
-  <div>
-    <jobb-list 
-      :data="listData"
-      :model="model"
-      modelKey="id_empresa"
-      :allColumns="allColumns"
-      cadastrarRoute="/unidades/formulario"
-      :rowsSelected="rowsSelected"
-      :search="searchData"
-      :listAction="listar"
-      :deleteAction="deletar"
-      :exportAction="exportar"
-      @handleSelectionChange="handleSelectionChange"
-    ></jobb-list>
-  </div>
+  <AdminLayout>
+    <PageBreadcrumb :pageTitle="currentPageTitle" />
+    <div
+      class="min-h-screen rounded-2xl border border-gray-200 bg-white px-5 py-7 dark:border-gray-800 dark:bg-white/[0.03] xl:px-10 xl:py-12"
+    >
+      <div class="mx-auto w-full max-w-[630px] text-center">
+        <el-table :data="state.tableData" style="width: 100%">
+          <el-table-column prop="date" label="Date" width="180" />
+          <el-table-column prop="name" label="Name" width="180" />
+          <el-table-column prop="address" label="Address" />
+        </el-table>
+      </div>
+    </div>
+  </AdminLayout>
 </template>
-<script>
-  import { mapState, mapActions } from "vuex";
-  import jobbModalCamposPersonalizados from "@/components/Jobb/Utils/CamposPersonalizados/ModalCamposPersonalizados.vue";
-  import {permissoesDaPagina} from "../../../components/Jobb/Utils/Util";
 
-  export default {
-    components: {
-      jobbModalCamposPersonalizados
-    },
-    computed: {
-      /***
-       * Returns a page from the searched data or the whole data. Search is performed in the watch section below
-       */
-      ...mapState("Empresa", {
-        listData: state => {
-          return state.listTable;
-        } 
-      }),
-      listData2(){
-        let self = this;
-        let state = self.$store.state.Empresa;
-        let result = [];
-        if (self.permissoesPagina()) {
-          result = state.listTable;
-        }      
-        //console.log(result);
-        return result;
-      }
-    },
-    data() {
-      return {
-        model: "Empresa",
-        allColumns: [
-          {
-            prop: "id_empresa",
-            label: "#",
-            minWidth: 50,
-            fixed: true
-          },
-          {
-            prop: "razao",
-            label: "Empresa",
-            minWidth: 200,
-            fixed: true
-          },
-          {
-            prop: "email",
-            label: "Email",
-            minWidth: 250,
-            fixed: true
-          },
-          {
-            prop: "cidade",
-            label: "Cidade",
-            minWidth: 150
-          },
-          {
-            prop: "fone",
-            label: "Telefone",
-            minWidth: 100
-          },
-          {
-            prop: "razao",
-            label: "Razão",
-            minWidth: 120
-          },
-          {
-            prop: "fantasia",
-            label: "Fantasia",
-            minWidth: 120
-          },
-          {
-            prop: "sigla",
-            label: "Sigla",
-            minWidth: 80
-          },
-          {
-            prop: "cnpj",
-            label: "CNPJ",
-            minWidth: 120
-          },
-          {
-            prop: "celular",
-            label: "Celular",
-            minWidth: 100
-          },
-          {
-            prop: "site",
-            label: "Site",
-            minWidth: 100
-          },
-          {
-            prop: "cep",
-            label: "Cep",
-            minWidth: 100
-          },
-          {
-            prop: "bairro",
-            label: "Bairro",
-            minWidth: 100
-          },
-          {
-            prop: "uf",
-            label: "Uf",
-            minWidth: 50
-          },
-          {
-            prop: "logradouro",
-            label: "Logradouro",
-            minWidth: 120
-          },
-          {
-            prop: "complemento",
-            label: "Complemento",
-            minWidth: 120
-          },
-          {
-            prop: "nfe_emitir_nota_fiscal",
-            label: "Nota Fiscal Eletrônica",
-            minWidth: 100
-          },
-          {
-            prop: "optante_simples_nacional",
-            label: "Optante do Simples Nacional",
-            minWidth: 120
-          },
-          {
-            prop: "codigo_cnae",
-            label: "Código de Atividade - CNAE",
-            minWidth: 120
-          },
-          {
-            prop: "cod_lc116",
-            label: "Código LC116 (Lei Complementar 116)",
-            minWidth: 80
-          },
-          {
-            prop: "cod_servico_municipal",
-            label: "Código de Serviço no Município (em algumas cidades, repete-se o cnae)",
-            minWidth: 80
-          },
-          {
-            prop: "ir_tax_rate",
-            label: "IR %",
-            minWidth: 100
-          },
-          {
-            prop: "csll_tax_rate",
-            label: "CSLL %",
-            minWidth: 100
-          },
-          {
-            prop: "pis_tax_rate",
-            label: "PIS %",
-            minWidth: 100
-          },
-          {
-            prop: "cofins_tax_rate",
-            label: "COFINS %",
-            minWidth: 100
-          },
-          {
-            prop: "inss_tax_rate",
-            label: "INSS %",
-            minWidth: 100
-          },
-          {
-            prop: "iss_tax_rate",
-            label: "ISS %",
-            minWidth: 100
-          },
-          {
-            prop: "nfe_serie",
-            label: "Série (Ex.: A)",
-            minWidth: 120
-          },
-          {
-            prop: "nfe_rps_atual",
-            label: "Número do RPS atual (Se não usar rps coloque 1)",
-            minWidth: 120
-          },
-          {
-            prop: "nfe_lote_atual",
-            label: "Número do lote atual (Se não usar rps coloque 1)",
-            minWidth: 120
-          }
-        ],
-        searchData: [],
-        rowsSelected: []
-      };
-    },
-    methods: {
-      // ...mapActions("Empresa", ["listar", "deletar", "exportar"]),
-      ...mapActions({
-        // listar:"Empresa/listar", 
-        listar:"Empresa/listarTable", 
-        deletar:"Empresa/deletar", 
-        exportar:"Empresa/exportar",
-        limparLista:"Empresa/limparListaTable",
-      }),
+<script setup>
+import PageBreadcrumb from '@/components/common/PageBreadcrumb.vue'
+import AdminLayout from '@/components/layout/AdminLayout.vue'
+import { onMounted, reactive, ref } from 'vue'
+import { useStore } from 'vuex'
 
-      handleSelectionChange(val) {
-        this.multipleSelection = val;
-      },
+const store = useStore()
 
-      permissoesPagina(des = 'Consultar',mod = 'empresa', contr = 'Cadastro'){
-        let self = this;
-        return permissoesDaPagina(self, {descricao: des, module: mod}, contr);
-      }
-    },
-    destroyed(){
-      this.limparLista();
-    }
-  };
+const state = reactive({
+  companies: '',
+  tableData:[
+    {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  ]
+})
+
+async function listCompany() {
+  try {
+    let result = await store.dispatch('Empresa/listar', {})
+  } catch (error) {
+    console.log('Erro ao listar unidades', error)
+  }
+}
+
+onMounted(() => {
+  listCompany()
+})
 </script>
-<style>
-  .card .card-body {
-    padding: 6px 15px 6px 15px;
-  }
 
-  input::-webkit-input-placeholder {
-    color: rgb(68, 68, 68) !important;
-  }
-</style>
+<style></style>
